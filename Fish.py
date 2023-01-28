2307aef465c12308cfe40f3a3b09835e8e27cb42
DonzerHD <thomas.lemay59@outlook.fr>
2023-01-28 14:33:00 +0100

    ajout fonction verification

--- Fish.py
+++ Fish.py
@@ -9,28 +9,36 @@ class Fish:
     def move(self, world):
         old_x = self.x
         old_y = self.y
-        direction = rd.randint(0,3)
-        if direction == 0:  # Haut
-            self.y -= 1
-        elif direction == 1:  # Droite
-            self.x += 1
-        elif direction == 2:  # Bas
-            self.y += 1
-        elif direction == 3:  # Gauche
-            self.x -= 1
-        
-        # vérifications limite monde
-        if self.x < 0:
-            self.x = 0
-        if self.x >= world.cols:
-            self.x = world.cols - 1
-        if self.y < 0:
-            self.y = 0
-        if self.y >= world.rows:
-            self.y = world.rows - 1
-            
-        return old_x, old_y
-            
+        # Génère une liste de positions possibles pour le poisson de se déplacer
+        possible_positions = self.verification_move(world)
+        #Si il y a des positions possibles pour le poisson de se déplacer
+        if len(possible_positions) > 0:
+            # Choisit aléatoirement une position de la liste possible_positions
+            new_pos = rd.choice(possible_positions)
+            # Met à jour la grille en enlevant le poisson de sa position actuelle
+            # Met à jour les coordonnées du poisson
+            self.x = new_pos[0]
+            self.y = new_pos[1]
+            # Met à jour la grille en ajoutant le poisson à sa nouvelle position
+        return old_x , old_y
+    
+    def verification_move(self, world):
+        # Initialise une liste de positions possibles pour le poisson de se déplacer
+        possible_positions = []
+        #Boucle à travers les déplacements possibles pour le poisson (dx et dy peuvent prendre les valeurs -1, 0, 1)
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                # Calcul les nouvelles coordonnées du poisson en utilisant l'opérateur modulo pour s'assurer 
+                # qu'elles restent dans les limites de la grille en bouclant sur les bords
+                x = (self.x + dx) % world.rows
+                y = (self.y + dy) % world.cols 
+                # Vérifie si la case cible ne contient pas un requin ou un autre poisson
+                if world.is_empty(x, y):
+                    # Ajoute les coordonnées de la case cible à la liste de positions possibles
+                    possible_positions.append((x, y))
+        # Retourne la liste de positions possibles
+        return possible_positions
+           
     def reproduction(self, world, old_x , old_y,day):
         if world.is_empty(old_x, old_y):
             if day % 4 == 0:
